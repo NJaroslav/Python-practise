0a2ebb77429edd7c3953d84c462472b31ff0b149
NJaroslav <108277031+NJaroslav@users.noreply.github.com>
2022-12-24 17:48:43 +0300

    added 4.7 solution

---
+++ 04_data_types/task4_7.py
@@ -0,0 +1,15 @@
+'''
+
+Convert MAC address in mac string to binary string like this: 101010101010101010111011101110111100110011001100
+
+Print the resulting new string to the standard output (stdout) using print.
+
+Restriction: All tasks must be done using the topics covered in this and previous chapters.
+
+mac = "AAAA:BBBB:CCCC"
+
+'''
+
+mac = "AAAA:BBBB:CCCC"
+bin_mac = "{:b}".format(int(mac.replace(":", ""), 16))
+print(bin_mac)
