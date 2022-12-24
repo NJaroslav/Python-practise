e5df5f1b2624c960f1fce4292a25a40df319758d
NJaroslav <108277031+NJaroslav@users.noreply.github.com>
2022-12-24 17:25:48 +0300

    added 4.1 solution

---
+++ 04_data_types/task4_1.py
@@ -0,0 +1,15 @@
+'''
+
+Using the prepared nat string, get a new string where the FastEthernet interface is replaced with GigabitEthernet.
+
+Print the resulting new string to the standard output (stdout) using print.
+
+Restriction: All tasks must be done using the topics covered in this and previous chapters.
+
+nat = "ip nat inside source list ACL interface FastEthernet0/1 overload"
+
+'''
+
+nat = "ip nat inside source list ACL interface FastEthernet0/1 overload"
+nat_replaced = nat.replace("Fast", "Gigabit")
+print(nat_replaced)
