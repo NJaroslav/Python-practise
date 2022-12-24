0535fe2f50d59c8cf377c1667dc46cba22bcf22e
NJaroslav <jaroslav.questions@gmail.com>
2022-12-24 17:36:10 +0300

    solved 4.2

---
+++ 04_data_types/task4_2.py
@@ -0,0 +1,15 @@
+'''
+
+Convert string in mac variable from XXXX:XXXX:XXXX format to XXXX.XXXX.XXXX format.
+
+Print the resulting new string to the standard output (stdout) using print.
+
+Restriction: All tasks must be done using the topics covered in this and previous chapters.
+
+mac = "AAAA:BBBB:CCCC"
+
+'''
+
+mac = "AAAA:BBBB:CCCC"
+mac_replaced = mac.replace(":", ".")
+print(new_mac)
