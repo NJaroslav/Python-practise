ae316d67c52105f8f2a2c4ac3ca801168bf73043
NJaroslav <108277031+NJaroslav@users.noreply.github.com>
2022-12-25 22:31:24 +0300

    added a description for 5.3(a) solution

--- 05_basic_scripts/task5_3_a.py
+++ 05_basic_scripts/task5_3_a.py
@@ -1,3 +1,42 @@
+'''
+Task #5.3
+
+The script should prompt the user for input:
+
+- interface mode (access/trunk)
+- interface number (Gi0/3)
+- VLAN number (for trunk mode, a list of VLANs will be entered)
+
+Depending on the selected mode,print corresponding access or trunk
+configuration on stdout.
+
+The output should first print the interface line and the interface number, 
+and then the corresponding template in which the VLAN number
+(or the list of VLANs) is inserted.
+'''
+
+'''
+Task #5.3(a)
+Copy and change the script from task 5.3 in such a way that,
+depending on the selected mode, different questions were asked 
+in the request for the VLAN number or VLAN list:
+
+- for access: ‘Enter VLAN number:’
+- for trunk: ‘Enter the allowed VLANs:’
+
+access_template = [
+    "switchport mode access", "switchport access vlan {}",
+    "switchport nonegotiate", "spanning-tree portfast",
+    "spanning-tree bpduguard enable"
+]
+
+trunk_template = [
+    "switchport trunk encapsulation dot1q", "switchport mode trunk",
+    "switchport trunk allowed vlan {}"
+]
+'''
+
+
 access_template = [
     "switchport mode access", "switchport access vlan {}",
     "switchport nonegotiate", "spanning-tree portfast",
