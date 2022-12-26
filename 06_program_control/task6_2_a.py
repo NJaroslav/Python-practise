c1f5ecc17003ce6a5a40b1a21779a4433a49fdc8
NJaroslav <108277031+NJaroslav@users.noreply.github.com>
2022-12-26 20:59:01 +0300

    added description for 6.2(a) solution

--- 06_program_control/task6_2_a.py
+++ 06_program_control/task6_2_a.py
@@ -1,3 +1,21 @@
+'''
+Task #6.2a
+
+Make a copy of the code from the task 6.2.
+
+Add verification of the entered IP address. An IP address is considered correct if it:
+
+- consists of 4 numbers (not letters or other symbols)
+- numbers are separated by a dot
+- every number in the range from 0 to 255
+
+If the IP address is incorrect, print the message: ‘Invalid IP address’
+The message “Invalid IP address” should be printed only once, even if several points above are not met.
+
+Restriction: All tasks must be done using the topics covered in this and previous chapters.
+'''
+
+
 ip = input("Inter an IP address: ")
 ip_bytes = ip.split(".")
 is_valid_ip = len(ip_bytes) == 4
