22c849f12e7c19636e0d87ae53be96dc613748a1
NJaroslav <108277031+NJaroslav@users.noreply.github.com>
2022-12-26 21:00:10 +0300

    added description for 6.2(b) solution

--- 06_program_control/task6_2_b.py
+++ 06_program_control/task6_2_b.py
@@ -1,3 +1,13 @@
+'''
+Task #6.2b
+
+Make a copy of the code from the task 6.2a.
+
+Add this functionality: If the address was entered incorrectly, request the address again.
+
+Restriction: All tasks must be done using the topics covered in this and previous chapters.
+'''
+
 is_valid_ip = False
 
 while not is_valid_ip:
