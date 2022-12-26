78bc801499248b86520cd75052dbcfa218f576e3
NJaroslav <jaroslav.questions@gmail.com>
2022-12-26 20:20:58 +0300

    added 6.2 solution

---
+++ 06_program_control/task6_2_a.py
@@ -0,0 +1,25 @@
+ip = input("Inter an IP address: ")
+ip_bytes = ip.split(".")
+is_valid_ip = len(ip_bytes) == 4
+
+for byte in ip_bytes:
+    is_valid_ip  = byte.isdigit() and is_valid_ip and 0 <= int(byte) <= 255  
+
+if is_valid_ip:
+
+    first_byte = int(ip.split(".")[0])
+
+    if 1 <= first_byte <= 233:
+        print("unicast")
+    elif 224 <= first_byte <= 239:
+        print("multicast")
+    elif ip ==  "0.0.0.0":
+        print("unassigned")
+    elif ip == "255.255.255.255":
+        print("local broadcast")
+    else:
+        print("unused")
+else:
+    print("Invalid IP address")
+
+
