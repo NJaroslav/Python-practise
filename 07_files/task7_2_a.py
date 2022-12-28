10f3cc0cf0be0ea129c905f7590c1e931a47a4af
NJaroslav <108277031+NJaroslav@users.noreply.github.com>
2022-12-28 21:20:19 +0300

    added description for 7.2(a) solution

--- 07_files/task7_2_a.py
+++ 07_files/task7_2_a.py
@@ -1,3 +1,17 @@
+'''
+Task #7.2(a)
+
+Make a copy of the code from the task 7.2.
+
+Add this functionality: The script should not print to the stdout commands, which contain words from the ignore list. 
+The script should also not print lines that begin with !.
+
+Check the script on the config_sw1.txt configuration file. The filename is passed as an argument to the script.
+
+ignore = ["duplex", "alias", "Current configuration"]
+'''
+
+
 from sys import argv
 
 file_name = argv[1]
