8760ef463801952a87eff330f42005a214e9a6b9
NJaroslav <108277031+NJaroslav@users.noreply.github.com>
2022-12-28 21:18:39 +0300

    added description for 7.2 solution

--- 07_files/task7_2.py
+++ 07_files/task7_2.py
@@ -1,3 +1,15 @@
+'''
+Task #7.2
+
+Create a script that will process the config_sw1.txt configuration file. 
+The filename is passed as an argument to the script.
+
+The script should return to the stdout commands from the passed configuration file,
+excluding lines that start with ‘!’.
+
+There should be no blank lines in the output.
+'''
+
 from sys import argv
 
 file_name = argv[1]
